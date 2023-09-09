62f300a195c88f040a3ac4af1d8075ce272f8257
satrioo <satrioari43@gmail.com>
2023-09-09 17:53:12 +0700

    init store

---
+++ src/components/Navbar.js
@@ -0,0 +1,143 @@
+import React from "react";
+import { useSelector } from 'react-redux'
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+ 
+function NavbarDefault() {
+  const [openNav, setOpenNav] = React.useState(false);
+ 
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false),
+    );
+  }, []);
+ 
+  const navList = (
+    <ul className=" text-black mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Home
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Product
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          About 
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Contact
+        </a>
+      </Typography>
+    </ul>
+  );
+ 
+  return (
+    <>
+    <div className=" h-[72px] mb-6"></div>
+    <div className="fixed w-screen top-0 left-[-7px] bg-white border-0">
+    <Navbar className=" text-black mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4 overflow-hidden  border-0">
+      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          className="mr-4 cursor-pointer py-1.5 font-medium"
+        >
+          Online Store
+        </Typography>
+        <div className="hidden lg:block">{navList}</div>
+        <div>
+        <Button variant="gradient" size="sm" className="hidden lg:inline-block text-black text-sm">
+          <span>View Cart</span>
+        </Button>
+        <Button variant="gradient" size="sm" className="hidden lg:inline-block text-black text-sm">
+          <span>Login</span>
+        </Button>
+        </div>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </IconButton>
+      </div>
+      <MobileNav open={openNav}>
+        <div className="container mx-auto">
+          {navList}
+          <Button variant="gradient" size="sm" fullWidth className="mb-2">
+            <span>View Cart </span>
+          </Button>
+          <Button variant="gradient" size="sm" fullWidth className="mb-2">
+          <i className="fa fa-cart-shopping mr-1"></i><span>Login</span>
+          </Button>
+        </div>
+      </MobileNav>
+    </Navbar>
+    </div>
+    </>
+  );
+}
+
+export default NavbarDefault
---
+++ src/components/Product.js
@@ -0,0 +1,188 @@
+import React, { useState, useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { addCart } from "../redux/action";
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+// import { Link } from "react-router-dom";
+import Link from "next/link";
+
+const Products = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+  let componentMounted = true;
+
+  // const dispatch = useDispatch();
+
+  // const addProduct = (product) => {
+  //   dispatch(addCart(product))
+  // }
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products/");
+      if (componentMounted) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
+      }
+
+      return () => {
+        componentMounted = false;
+      };
+    };
+
+    getProducts();
+  }, []);
+
+  const Loading = () => {
+    return (
+      <>
+        <div className="col-12 py-5 text-center">
+          <Skeleton height={40} width={560} />
+        </div>
+        <div className=" grid grid-cols-4 grid-flow-row gap-7">
+          <div className=" w-full">
+            <Skeleton height={592} />
+          </div>
+          <div className=" w-full">
+            <Skeleton height={592} />
+          </div>
+          <div className=" w-full">
+            <Skeleton height={592} />
+          </div>
+          <div className=" w-full">
+            <Skeleton height={592} />
+          </div>
+          <div className=" w-full">
+            <Skeleton height={592} />
+          </div>
+          <div className=" w-full">
+            <Skeleton height={592} />
+          </div>
+          <div className=" w-full">
+            <Skeleton height={592} />
+          </div>
+          <div className=" w-full">
+            <Skeleton height={592} />
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  const filterProduct = (cat) => {
+    const updatedList = data.filter((item) => item.category === cat);
+    setFilter(updatedList);
+  };
+  const ShowProducts = () => {
+    return (
+      <div>
+        <div className="buttons text-center py-5">
+          <button
+            className="btn btn-outline-dark btn-sm m-2"
+            onClick={() => setFilter(data)}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-outline-dark btn-sm m-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Men's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark btn-sm m-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            Women's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark btn-sm m-2"
+            onClick={() => filterProduct("jewelery")}
+          >
+            Jewelery
+          </button>
+          <button
+            className="btn btn-outline-dark btn-sm m-2"
+            onClick={() => filterProduct("electronics")}
+          >
+            Electronics
+          </button>
+        </div>
+
+        <div className=" grid grid-cols-4 grid-flow-row gap-7 max-w-screen-xl mx-auto">
+          {filter.map((product) => {
+            return (
+              <div
+                id={product.id}
+                key={product.id}
+                className="w-full border rounded"
+              >
+                <div
+                  className="card text-center h-100 px-4 py-6"
+                  key={product.id}
+                >
+                  <figure className=" h-[400px] overflow-hidden flex items-center">
+                    <img
+                      className="card-img-top p-5 object-fill"
+                      src={product.image}
+                      alt="Card"
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {product.title.substring(0, 12)}...
+                    </h5>
+                    <p className="card-text">
+                      {product.description.substring(0, 90)}...
+                    </p>
+                  </div>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item lead">$ {product.price}</li>
+                    {/* <li className="list-group-item">Dapibus ac facilisis in</li>
+                    <li className="list-group-item">Vestibulum at eros</li> */}
+                  </ul>
+                  <div className="card-body mt-4">
+                    <Link
+                      href={"/product/" + product.id}
+                      className="  bg-gray-500 text-white px-3 inline-flex items-center h-7 text-sm rounded"
+                    >
+                      Buy Now
+                    </Link>
+                    <button
+                      className=" bg-gray-500 text-white px-3 py-1 text-sm rounded inline-flex items-center h-7 ml-1"
+                      onClick={() => addProduct(product)}
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div>
+      <div className=" mx-auto my-7 max-w-screen-xl">
+        <div className="row">
+          <h2 className="display-5 text-center text-lg mb-4">
+            Latest Products
+          </h2>
+          <hr />
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
